04f58a1cd6897ea3d1279e550e4c71210b5d734d
paulapreuss <paulavpreuss@outlook.com>
2025-04-14 11:52:54 +0200

    Update project status on select toggle

--- offgridplanner/projects/urls.py
+++ offgridplanner/projects/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path("<int:proj_id>", projects_list, name="projects_list"),
     path("duplicate/<int:proj_id>", project_duplicate, name="project_duplicate"),
     path("delete/<int:proj_id>", project_delete, name="project_delete"),
+    path("update_project_status", update_project_status, name="update_project_status"),
     path(
         "export_results/<int:proj_id>",
         export_project_results,

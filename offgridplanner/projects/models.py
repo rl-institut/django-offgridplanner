a6a171a219483d8c8341fd0ccb422d37dd18ff76
paulapreuss <paulavpreuss@outlook.com>
2025-08-19 16:11:39 +0200

    Add migration

--- offgridplanner/projects/models.py
+++ offgridplanner/projects/models.py
@@ -81,9 +81,19 @@ class Project(models.Model):
 class SiteExploration(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     exploration_id = models.TextField(null=True)
-    consumer_count_min = models.PositiveSmallIntegerField(default=100,
-                                                          validators=[MinValueValidator(31), MaxValueValidator(500)])
-    diameter_max = models.FloatField(default=500, validators=[MinValueValidator(0.1), MaxValueValidator(10000)])
-    distance_from_grid_min = models.FloatField(default=60000, validators=[MinValueValidator(20000), MaxValueValidator(120000)])
-    match_distance_max = models.FloatField(default=5000, validators=[MinValueValidator(100), MaxValueValidator(20000)])
+    consumer_count_min = models.PositiveSmallIntegerField(
+        default=100, validators=[MinValueValidator(31), MaxValueValidator(500)]
+    )
+    diameter_max = models.FloatField(
+        default=500, validators=[MinValueValidator(0.1), MaxValueValidator(10000)]
+    )
+    distance_from_grid_min = models.FloatField(
+        default=60000, validators=[MinValueValidator(20000), MaxValueValidator(120000)]
+    )
+    match_distance_max = models.FloatField(
+        default=5000, validators=[MinValueValidator(100), MaxValueValidator(20000)]
+    )
     latest_exploration_results = models.JSONField(null=True)
+
+    def __str__(self):
+        return f"{self.exploration_id}"

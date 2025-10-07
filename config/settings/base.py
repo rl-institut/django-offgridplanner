5f3ee7bef1d277b9fd430c7ddf57907c73b12a00
paulapreuss <paulavpreuss@outlook.com>
2025-10-07 14:48:55 +0200

    Replace demand profile data

--- config/settings/base.py
+++ config/settings/base.py
@@ -59,7 +59,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # External data files
 DATA_DIR = Path(APPS_DIR) / "static" / "data"
-FULL_PATH_PROFILES = Path(DATA_DIR) / "1-hour_mean_365_days_all_users.parquet"
+FULL_PATH_PROFILES = (
+    Path(DATA_DIR) / "1-hour_mean_365_days_all_users_simplified.parquet"
+)
 
 # URLS
 # ------------------------------------------------------------------------------

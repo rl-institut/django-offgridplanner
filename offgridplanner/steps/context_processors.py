3b342553c02cb235ddc8e9dcbc510104b66d5e2f
paulapreuss <paulavpreuss@outlook.com>
2025-12-18 11:43:57 +0100

    Fix resolver_match AttributeError on locale redirect

--- offgridplanner/steps/context_processors.py
+++ offgridplanner/steps/context_processors.py
@@ -8,8 +8,9 @@ def current_project(request):
     Add the current project name to all templates, if available.
     """
     project = None
-    if hasattr(request, "resolver_match"):
-        project_id = request.resolver_match.kwargs.get("proj_id")
+    rm = request.resolver_match
+    if rm and rm.kwargs:
+        project_id = rm.kwargs.get("proj_id")
         if project_id:
             # pass if the project does not exist (may be the case for step 1, before it is saved to the database)
             with contextlib.suppress(Project.DoesNotExist):

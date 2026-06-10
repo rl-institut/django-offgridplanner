856e8adad8af5d129d6a3e785d76090c689ff90a
paulapreuss <paulavpreuss@outlook.com>
2026-06-10 09:58:30 +0200

    Add missing translation utils to strings

--- offgridplanner/static/js/pages/demand_estimation.js
+++ offgridplanner/static/js/pages/demand_estimation.js
@@ -172,29 +172,29 @@ const colors = {
 };
 
 var layout = {
-    font: { size: 14 },
-    autosize: true,
-    xaxis: {
-        title: 'Hour of the day',
-        hoverformat: '.1f',
-        titlefont: { size: 16 },
-        tickfont: { size: 14 },
-    },
-    yaxis: {
-        title: 'Demand (kW)',
-        hoverformat: '.1f',
-        titlefont: { size: 16 },
-        tickfont: { size: 14 },
-    },
-    legend: {
-        orientation: 'h',
-        x: 0,
-        y: -0.3,
-        xanchor: 'left',
-        yanchor: 'top',
-        traceorder: 'normal' // Ensure legendrank is honored
-    }
-};
+        font: { size: 14 },
+        autosize: true,
+        xaxis: {
+            title: gettext('Hour of the day'),
+            hoverformat: '.1f',
+            titlefont: { size: 16 },
+            tickfont: { size: 14 },
+        },
+        yaxis: {
+            title: gettext('Demand (kW)'),
+            hoverformat: '.1f',
+            titlefont: { size: 16 },
+            tickfont: { size: 14 },
+        },
+        legend: {
+            orientation: 'h',
+            x: 0,
+            y: -0.3,
+            xanchor: 'left',
+            yanchor: 'top',
+            traceorder: 'normal' // Ensure legendrank is honored
+        }
+    };
 
 
 function buildPlot(data) {
@@ -229,7 +229,7 @@ function buildPlot(data) {
             x: x,
             y: AppState.total_demand_raw,
             mode: 'lines',
-            name: 'Total Demand',
+            name: gettext('Total Demand'),
             line: { color: colors.total, width: 3, shape: 'spline' },
             visible: true, // Initially visible
             legendrank: 0
@@ -239,7 +239,7 @@ function buildPlot(data) {
             y: public_services,
             type: 'scatter',
             mode: 'lines',
-            name: 'Demand of Public Services',
+            name: gettext('Demand of Public Services'),
             stackgroup: 'one',
             fill: 'tonexty',
             hoverinfo: 'x+y',
@@ -252,7 +252,7 @@ function buildPlot(data) {
             y: enterprises,
             type: 'scatter',
             mode: 'lines',
-            name: 'Demand of Enterprises',
+            name: gettext('Demand of Enterprises'),
             stackgroup: 'one',
             fill: 'tonexty',
             hoverinfo: 'x+y',
@@ -265,7 +265,7 @@ function buildPlot(data) {
             y: households,
             type: 'scatter',
             mode: 'lines',
-            name: 'Demand of Households',
+            name: gettext('Demand of Households'),
             stackgroup: 'one',
             fill: 'tonexty',
             hoverinfo: 'x+y',
@@ -277,7 +277,7 @@ function buildPlot(data) {
             x: x,
             y: Average,
             mode: 'lines',
-            name: 'Average Household Profile',
+            name: gettext('Average Household Profile'),
             line: { color: colors.average, width: 2, shape: 'spline' },
             visible: false, // Initially hidden
             legendrank: 4
@@ -286,7 +286,7 @@ function buildPlot(data) {
             x: x,
             y: Very_High,
             mode: 'lines',
-            name: 'Very High Consumption',
+            name: gettext('Very High Consumption'),
             line: { color: colors.very_high, width: 1, shape: 'spline' },
             visible: 'legendonly',
             legendrank: 5
@@ -295,7 +295,7 @@ function buildPlot(data) {
             x: x,
             y: High,
             mode: 'lines',
-            name: 'High Consumption',
+            name: gettext('High Consumption'),
             line: { color: colors.high, width: 1, shape: 'spline' },
             visible: 'legendonly',
             legendrank: 6
@@ -304,7 +304,7 @@ function buildPlot(data) {
             x: x,
             y: Middle,
             mode: 'lines',
-            name: 'Middle Consumption',
+            name: gettext('Middle Consumption'),
             line: { color: colors.middle, width: 1, shape: 'spline' },
             visible: 'legendonly',
             legendrank: 7
@@ -313,7 +313,7 @@ function buildPlot(data) {
             x: x,
             y: Low,
             mode: 'lines',
-            name: 'Low Consumption',
+            name: gettext('Low Consumption'),
             line: { color: colors.low, width: 1, shape: 'spline' },
             visible: 'legendonly',
             legendrank: 8
@@ -322,7 +322,7 @@ function buildPlot(data) {
             x: x,
             y: Very_Low,
             mode: 'lines',
-            name: 'Very Low Consumption',
+            name: gettext('Very Low Consumption'),
             line: { color: colors.very_low, width: 1, shape: 'spline' },
             visible: 'legendonly',
             legendrank: 9
